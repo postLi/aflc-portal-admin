af51097ea034c2ce066e775afc53859e88937b51
何永青 <864887652@qq.com>
2018-11-27 15:32:39 +0800

    fix bug

--- src/views/carrier/logistics/manageGroup/index.vue
+++ src/views/carrier/logistics/manageGroup/index.vue
@@ -23,6 +23,10 @@
                     <el-input v-model="logisticsForm.mobile" maxlength="11">
                     </el-input>
                 </el-form-item>
+                <el-form-item label="固话：" prop="telNum">
+                    <el-input v-model="logisticsForm.telNum" maxlength="11">
+                    </el-input>
+                </el-form-item>
                 <el-form-item class="btnChoose" style="margin-left:0;">
                     <el-button type="primary" @click="handleSearch">搜索</el-button>
                     <el-button type="primary" @click="clearSearch">重置</el-button>
@@ -114,116 +118,114 @@
 <script>
 
 import '@/styles/identification.scss'
-import { getPointNetwork,PointNetworkStatus,deletePointNetwork } from '@/api/carrier/index.js'
+import { getPointNetwork, PointNetworkStatus, deletePointNetwork } from '@/api/carrier/index.js'
 import Pager from '@/components/Pagination/index'
 
 export default {
-    components:{
-        Pager
+  components: {
+      Pager
     },
-    data() {
-       
-        return {
-            defaultImg:'/static/default.png',//默认加载失败图片
-            loading:true,
-            totalCount:0,
-            page:1,
-            pagesize:20,
-            logisticsForm: {
-                pointName: '',//网点名称
-                address: '',//网点详细地址
-                name: '',//联系人
-                mobile: '',//手机号
-                telNum: '',//固定电话
+  data() {
+      return {
+          defaultImg: '/static/default.png', // 默认加载失败图片
+          loading: true,
+          totalCount: 0,
+          page: 1,
+          pagesize: 20,
+          logisticsForm: {
+              pointName: '', // 网点名称
+              address: '', // 网点详细地址
+              name: '', // 联系人
+              mobile: '', // 手机号
+              telNum: '' // 固定电话
             },
-            tableData: [],
-        };
+          tableData: []
+        }
     },
-    watch:{
+  watch: {
 
     },
-    mounted(){
-        this.firstblood();
-    },  
-    methods: {
-        handlePageChange(obj) {
-            this.page = obj.pageNum
-            this.pagesize = obj.pageSize
+  mounted() {
+      this.firstblood()
+    },
+  methods: {
+      handlePageChange(obj) {
+          this.page = obj.pageNum
+          this.pagesize = obj.pageSize
         },
-        firstblood(){
-            this.loading = true;
-            getPointNetwork(this.page,this.pagesize,this.logisticsForm).then(res=>{
+      firstblood() {
+          this.loading = true
+            getPointNetwork(this.page, this.pagesize, this.logisticsForm).then(res => {
                 // console.log(res)
-                this.tableData = res.data.list;
-                this.totalCount = res.data.totalCount;
-                this.loading = false;
+              this.tableData = res.data.list
+                this.totalCount = res.data.totalCount
+                this.loading = false
             })
         },
-        clearSearch(){
-            this.$refs.ruleForm.resetFields();
-            this.firstblood();
+      clearSearch() {
+          this.$refs.ruleForm.resetFields()
+            this.firstblood()
         },
-        //搜索
-        handleSearch(){
-            this.firstblood();
+        // 搜索
+      handleSearch() {
+          this.firstblood()
         },
-        //新增网点
-        handleNew(){
-            this.$router.push({name: '发布我的网点'});
+        // 新增网点
+      handleNew() {
+          this.$router.push({ name: '发布我的网点' })
         },
-        //修改
-        handleEdit(row) {   
-            console.log(row);
-            this.$router.push({name: '发布我的网点',query:{ data:row}});
+        // 修改
+      handleEdit(row) {
+          console.log(row)
+            this.$router.push({ name: '发布我的网点', query: { data: row }})
         },
-        handleInfo(row){
-            this.$router.push({name: '发布我的网点',query:{ data:row , type:'check'}});
+      handleInfo(row) {
+          this.$router.push({ name: '发布我的网点', query: { data: row, type: 'check' }})
         },
-        //删除网点
-        handleDelete(row) {
-            this.$confirm('确定要删除'+ row.pointName +' 该网点名吗？', '提示', {
-                confirmButtonText: '确定',
-                cancelButtonText: '取消',
-                type: 'warning'
-            }).then(()=>{
-                deletePointNetwork(row.id).then(res => {
-                    this.firstblood();
+        // 删除网点
+      handleDelete(row) {
+          this.$confirm('确定要删除' + row.pointName + ' 该网点名吗？', '提示', {
+              confirmButtonText: '确定',
+              cancelButtonText: '取消',
+              type: 'warning'
+            }).then(() => {
+              deletePointNetwork(row.id).then(res => {
+                  this.firstblood()
                 }).catch(err => {
-                    this.$message({
-                        type: 'info',
-                        message: '操作失败，原因：' + errorInfo ? errorInfo : err.text
+                  this.$message({
+                      type: 'info',
+                      message: '操作失败，原因：' + errorInfo ? errorInfo : err.text
                     })
                 })
             }).catch(() => {
-                this.$message({
-                    type: 'info',
-                    message: '已取消'
+              this.$message({
+                  type: 'info',
+                  message: '已取消'
                 })
             })
         },
-        //更改状态
-        handleStatus(row) {
-            console.log(row);
+        // 更改状态
+      handleStatus(row) {
+          console.log(row)
             PointNetworkStatus(row.id).then(res => {
-                console.log(res)
-                this.firstblood();
-            }).catch(err=>{
-                this.$message({
-                    type: 'info',
-                    message: '操作失败，原因：' + err.text ? err.text : err
+              console.log(res)
+              this.firstblood()
+            }).catch(err => {
+              this.$message({
+                  type: 'info',
+                  message: '操作失败，原因：' + err.text ? err.text : err
                 })
             })
         },
-        handleClickMore(){
-            this.$refs['topSearch'].$el.classList.add('longSearchBox')
+      handleClickMore() {
+          this.$refs['topSearch'].$el.classList.add('longSearchBox')
         },
-        setShort(){
-            this.$refs['topSearch'].$el.classList.remove('longSearchBox')
-
+      setShort() {
+          this.$refs['topSearch'].$el.classList.remove('longSearchBox')
         }
 
-    },
-  
+    }
+
 }
 </script>
 
